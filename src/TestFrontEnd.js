7c3d5c7bf349d62eb136afe7ec924f2a34c9d968
YurrialSG <yuriinternacional86@gmail.com>
2019-11-26 11:23:07 -0300

    buscando dados d API

--- src/TestFrontEnd.js
+++ src/TestFrontEnd.js
@@ -1,40 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { KeyboardArrowLeft, KeyboardArrowRight } from '@material-ui/icons/';
-// import axios from 'axios';
+import axios from 'axios';
 import './TestFrontEnd.css';
 
-const hanbleSubmitRight = () => {
-  console.log("Pressionou botão Direito")
-
-}
-
-const hanbleSubmitLeft = () => {
-  console.log("Pressionou botão Esquerdo")
-}
-
-
 function TestFrontEnd() {
-  return (
-    <main>
-      <div className="Artboard">
-        <span className="Signos">Signos</span>
-        <div className="Rectangle-12-Copy"><hr /></div>
-        <div className="Rectangle-13-Copy-1">
-          <div className="Rectangle-13-Copy-2">
-            <span className="Combined-Shape" onClick={hanbleSubmitLeft}><KeyboardArrowLeft /></span>
-            <img className="aries" src={require('./images/aries.svg')} alt="logo" ></img>
-            <span className="ries">ÀRIES</span>
-            <span className="Combined-Shape-Copy" onClick={hanbleSubmitRight}><KeyboardArrowRight /></span>
-            <span>
-              <p className="O-trabalho-exige-de">
-                O trabalho exige de você dinamismo e movimento. É um dia agitado para concluir as tarefas, mas muito produtivo. É necessário dar atenção especial às expectativas e situações que mexem com o seu emocional. As responsabilidades são altas, mas são favoráveis para o seu desenvolvimento.
-            </p>
-            </span>
+
+  const [horoscopes, setHoroscopes] = useState([]);
+  const [position, setPosition] = useState(4);
+  const [load, setLoad] = useState(false);
+  const [error, setError] = useState('');
+
+  const hanbleSubmitRight = () => {
+    console.log("Pressionou botão Direito")
+
+  }
+
+  const hanbleSubmitLeft = () => {
+    console.log("Pressionou botão Esquerdo")
+  }
+
+  useEffect(() => {
+    axios.get('https://horoscope.suaradionanet.net/api/horoscope', {
+      headers: {
+        'x-api-key': 'OiU(!kh4Fj8!dZP(u)9Fr'
+      }
+    })
+      .then(res => {
+        setHoroscopes(res.data);
+        console.log(res.data)
+        setLoad(true);
+      })
+      .catch(err => {
+        setError(err.message);
+        setLoad(true)
+      })
+  }, []);
+
+
+  if (load) {
+    return (
+      <main>
+        {/* {console.log(JSON.stringify(horoscopes[position]))} */}
+        {/* {console.log(JSON.stringify(horoscopes[position].predictions[0].description))} */}
+        <div className="Artboard">
+          <span className="Signos">Signos</span>
+          <div className="Rectangle-12-Copy"><hr /></div>
+          <div className="Rectangle-13-Copy-1">
+            <div className="Rectangle-13-Copy-2">
+              <span className="Combined-Shape" onClick={hanbleSubmitLeft}><KeyboardArrowLeft /></span>
+              <img className="aries" src={require(`./images/${horoscopes[position].slug}.svg`)} alt="logo" ></img>
+              <span className="ries">{horoscopes[position].description}</span>
+              <span className="Combined-Shape-Copy" onClick={hanbleSubmitRight}><KeyboardArrowRight /></span>
+              <span>
+                <p className="O-trabalho-exige-de">
+                  {horoscopes[position].predictions[0].description}
+                </p>
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  } else {
+    return (
+      <div>
+        Loading...
+        </div>
+    );
+  }
+
 }
 
 export default TestFrontEnd;
